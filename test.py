e66a1fe21725e37703da44fd4864dd9158ce4e17
ahana21 <koua280@aa.edu>
2024-12-05 09:06:02 -0700

    I started the file

---
+++ test.py
@@ -0,0 +1,10 @@
+# Simple Python script to print "Hello, World!" and basic addition
+def main():
+    print("Hello, World!")
+    a = 5
+    b = 3
+    print(f"The sum of {a} and {b} is {a + b}")
+
+if __name__ == "__main__":
+    main()
+
